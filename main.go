d05019acede79f0350290e7b746b689ac3ab69a7
Narapong Hongthai <138201998+Phadoo131@users.noreply.github.com>
2023-08-09 21:18:20 +0700

    Update main.go

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 func SliceToCh (num []int) <-chan int{
 	out := make(chan int)
@@ -17,9 +18,12 @@ func SliceToCh (num []int) <-chan int{
 func AddNumberToSlice (in <-chan int) <-chan int{
 	out := make(chan int)
 	app := []int{}
+	var mu sync.Mutex
 	go func(){
 		for x := range in{
+			mu.Lock()
 			app = append(app, x)
+			mu.Unlock()
 		}
 		app = append(app, app...)
 		for _,y := range app{
@@ -32,10 +36,16 @@ func AddNumberToSlice (in <-chan int) <-chan int{
 
 func main(){
 	num := []int{1, 2, 3, 4, 5}
+	var wait sync.WaitGroup
 
 	stage1 := SliceToCh(num)
 	stage2 := AddNumberToSlice(stage1)
+
+	wait.Add(2)
 	for n := range stage2{
+		defer wait.Done()
 		fmt.Println(n)
 	}
+
+	wait.Wait()	
 }
